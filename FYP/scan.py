c731a747ed70eb3ba3bf521d821bc9aef8690467
Raissa Pululu <K00260175@student.lit.ie>
2024-01-17 23:03:29 +0000

    updating text

--- FYP/scan.py
+++ FYP/scan.py
@@ -4,7 +4,7 @@ import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 from time import sleep
 
-def scan():
+def scan(user_input, output_callback):
 
         #Include the buzzer pin
         buzzer = 19
