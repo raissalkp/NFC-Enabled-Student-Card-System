7a9419d304ea55e8b33e9891fb2da7be83c0ad4f
raiss <k00260175@student.tus.ie>
2024-02-07 15:16:56 +0000

    api with online callable

--- FYP/app.py
+++ FYP/app.py
@@ -29,4 +29,7 @@ def get_recent_attendance():
     return jsonify(records)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
+    # http://192.168.1.10:5000/attendance/last_2_hours
+    # insert raspberry pi IP address
