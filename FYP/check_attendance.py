f8b821bedcdba0485cde2cda827f667f63256e03
Raissa Pululu <K00260175@student.lit.ie>
2024-03-16 16:31:33 +0000

    sample new gui attendance

--- FYP/check_attendance.py
+++ FYP/check_attendance.py
@@ -4,17 +4,13 @@ from mfrc522 import SimpleMFRC522
 import mysql.connector
 import I2C_LCD_driver as LCD
 
-terminate_check_attendance = False  # Flag variable to control the loop
 
-
-def check_attendance(output_callback):
-    global terminate_check_attendance  # Access the flag variable from the outer scope
-
+def check_attendance(user_input, output_callback):
     try:
         read = SimpleMFRC522()
         lcd = LCD.lcd()
 
-        while not terminate_check_attendance:  # Continue looping until termination flag is set
+        while True:
             lcd.lcd_clear()
             lcd.lcd_display_string("Place Card to ", 1, 0)
             lcd.lcd_display_string("record attendance", 2, 0)
@@ -41,15 +37,23 @@ def check_attendance(output_callback):
                 else:
                     lcd.lcd_display_string("User does not exist.")
                     output_callback("User does not exist.")
+                    lcd.lcd_clear()
+                    lcd.lcd_display_string('Continue? (y/n)', 1, 0)
+                    output_callback("Continue? (y/n)")
+                    continue_response = input("Continue? (y/n): ")
+                    if continue_response.lower() != 'y':
+                        continue
 
             time.sleep(2)
+            lcd.lcd_clear()
+            lcd.lcd_display_string('Continue? (y/n)', 1, 0)
+            output_callback("Continue? (y/n)")
+            continue_response = input("Continue? (y/n): ")
+            if continue_response.lower() != 'y':
+                break
+
     except Exception as e:
         output_callback("Error: " + str(e))
     finally:
         GPIO.cleanup()
     GPIO.cleanup()
-
-
-def stop_check_attendance():
-    global terminate_check_attendance
-    terminate_check_attendance = True  # Set the flag to stop the loop
